003b575e0c123ba01c0d86aa0e7311ae4382212c
Victoria Meah <victoriameah@hotmail.com>
2022-01-06 12:10:57 +0000

    task 3

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ export function addToBeginning(array, item) {
 }
 
 // Immutably insert an item at a specific position/index within an array
-export function insertItem(array, item, index) {}
+export function insertItem(array, item, index) {
+  return [...array.slice(0,index),item,...array.slice(index)]
+}
 
 // Immutably replace an item at a specific position/index within an array
 export function replaceItem(array, item, index) {}
